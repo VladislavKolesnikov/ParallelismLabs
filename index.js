9c1b5b26e67afe596b75c59d9aba134d6f7995ff
Vladislav Kolesnikov <vladislav.kolesnikov@osf-commerce.com>
2019-03-10 08:53:13 +0200

    delete unnecessary code

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-    console.log(req.query);
     res.send(`Hello ${req.query.name ? req.query.name : "World!"}`);
-    console.log(req.query);
 });
 
 app.listen(port, function() {
-    console.log("Example app listening on port 8080!");
+    console.log(`Example app listening on port ${port}`);
 });
